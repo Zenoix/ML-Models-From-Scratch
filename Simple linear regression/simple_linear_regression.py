7dd5aedf3923948de8f2213c9663188158b8f9c5
Zenoix <jeff.wang0296@gmail.com>
2021-02-07 17:26:39 +1300

    Worked on fit method

--- Simple linear regression/simple_linear_regression.py
+++ Simple linear regression/simple_linear_regression.py
@@ -7,13 +7,32 @@ class SimpleLinearRegression:
         self.iters = iterations
         self.weight = None
         self.bias = None
+        self.verbose = verbose
 
     def __str__(self):
-        output = f"SimpleLinearRegression: learning rate={self.l_rate}, iterations={self.iters}"
+        output = (
+            f"Simple Linear Regression Model: "
+            f"learning rate={self.l_rate}, iterations={self.iters}"
+        )
         return output
 
     def fit(self, X, y):
-        pass
+        self.weight = 0
+        self.bias = 0
+
+        for iteration in range(self.iters):
+            y_pred = self.weight*X+self.bias
+            dw = np.mean(-2 * X * (y - y_pred))
+            db = np.mean(-2 * (y - y_pred))
+            self.weight -= self.l_rate * dw
+            self.bias -= self.l_rate * db
+            if self.verbose and (not iteration % 10 or
+                                 iteration == self.iters - 1):
+                info = (
+                    f"Iteration {iteration}: "
+                    f"Weight={self.weight}, Bias={self.bias}"
+                )
+                print(info)
 
     def predict(self, X):
         pass
@@ -27,5 +46,8 @@ class SimpleLinearRegression:
     def intercept(self):
         return self.bias
 
-    def mse(self):
-        pass
+    def mse(self, y_true, y_hat):
+        return np.mean((y_true - self.weight*y_hat + self.bias) ** 2)
+
+
+a = SimpleLinearRegression()
