5b5543829bae5605c9f81dce23a4d3c01988c5da
Zenoix <jeff.wang0296@gmail.com>
2021-02-08 01:25:09 +1300

    Updated test

--- Simple linear regression/simple_linear_regression_test.py
+++ Simple linear regression/simple_linear_regression_test.py
@@ -7,26 +7,25 @@ import matplotlib.pyplot as plt
 from simple_linear_regression import SimpleLinearRegression
 
 # simulate linear data
-X, y = datasets.make_regression(
-    n_samples=100, n_features=1, noise=20, random_state=1)
+X, y, coef = datasets.make_regression(
+    n_samples=100, n_features=1, noise=20, coef=True)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1)
+    X, y, test_size=0.2)
+
+print(coef)
 
 # create the model
-regressor = SimpleLinearRegression()
-
+regressor = SimpleLinearRegression(grad_descent=False)
 # train the model
 regressor.fit(X_train, y_train)
-
-# model parameters
 print(regressor.params())
 
 # make predictions for mse
 y_hat = regressor.predict(X_test)
-print(regressor.mse(y_test, y_hat))
 
 # graph the regression line
 pred_line = regressor.predict(X)
 plt.scatter(X, y)
-plt.plot(X, pred_line, color='black', linewidth=1)
+plt.plot(X, pred_line, color='black', linewidth=2)
 plt.show()
+
