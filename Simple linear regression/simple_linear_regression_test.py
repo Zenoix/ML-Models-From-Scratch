9bc2b35b9303c24307014da33ffd398107d45c1c
Zenoix <jeff.wang0296@gmail.com>
2021-02-08 01:53:26 +1300

    Improved test file

--- Simple linear regression/simple_linear_regression_test.py
+++ Simple linear regression/simple_linear_regression_test.py
@@ -2,26 +2,40 @@
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 import matplotlib.pyplot as plt
+import random
 
 # import the model
 from simple_linear_regression import SimpleLinearRegression
 
-# simulate linear data
+# generate linear data
 X, y, coef = datasets.make_regression(
-    n_samples=100, n_features=1, noise=20, coef=True)
+    n_samples=1000, n_features=1, noise=20, coef=True)
+
+# need to randomly make negative data sometimes
+negative = random.choice([1, -1])
+y, coef = y * negative, coef * negative
+
+# data's coefficient
+print(coef)
+
+# split data for training and testing
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2)
 
-print(coef)
-
 # create the model
 regressor = SimpleLinearRegression(grad_descent=False)
+
 # train the model
 regressor.fit(X_train, y_train)
+
+# model parameters
 print(regressor.params())
 
 # make predictions for mse
 y_hat = regressor.predict(X_test)
+
+# print mse
+print(regressor.mse(y_test, y_hat))
 
 # graph the regression line
 pred_line = regressor.predict(X)
