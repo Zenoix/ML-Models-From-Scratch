91f5f56e4ab727cd022da4594fe364020e7738e5
Zenoix <jeff.wang0296@gmail.com>
2021-02-11 20:34:26 +1300

    Continued modifying old slr code to fit mlr

--- multi_linear_regression/mlr_model.py
+++ multi_linear_regression/mlr_model.py
@@ -2,55 +2,40 @@ import numpy as np
 
 
 class LinearRegression:
-    def __init__(self, learning_rate=0.01, iterations=1000,
-                 grad_descent=True, verbose=False):
+    def __init__(self, learning_rate=0.01, iterations=1000, verbose=False):
         # initialise hyperparameters
         self.l_rate = learning_rate
         self.iters = iterations
-        self.grad_descent = grad_descent
         self.verbose = verbose
         self.weight = None
         self.bias = None
 
     def fit(self, X, y):
         # initialise parameters
-        self.weight = 0
+        n_samples, n_features = X.shape
+        self.weight = np.zeros(n_features)
         self.bias = 0
 
         # Using gradient descent
-        if self.grad_descent:
-            for iteration in range(self.iters):
-                # prediction with current parameters
-                y_pred = self.weight * X + self.bias
+        for iteration in range(self.iters):
+            # prediction with current parameters
+            y_pred = self.weight * X + self.bias
 
-                # gradient descent differentiation
-                dw = np.mean(X * (y_pred - y))
-                db = np.mean(y_pred - y)
+            # gradient descent differentiation
+            dw = np.mean(X * (y_pred - y))
+            db = np.mean(y_pred - y)
 
-                # update rules
-                self.weight -= self.l_rate * dw
-                self.bias -= self.l_rate * db
+            # update rules
+            self.weight -= self.l_rate * dw
+            self.bias -= self.l_rate * db
 
-                if self.verbose and (not iteration % 100 or
-                                     iteration == self.iters - 1):
-                    info = (
-                        f"Iteration {iteration}: "
-                        f"Weight={self.weight}, Bias={self.bias}"
-                    )
-                    print(info)
-
-        # Without gradient descent
-        else:
-            N = len(X)
-            y_mean = y.mean()
-            X_mean = X.mean()
-            sum_Xy = (y * X).sum()
-            Xy_byN = (y.sum() * X.sum()) / N
-            sum_X_square = (X * X).sum()
-            X_square_byN = (X.sum() * X.sum()) / N
-
-            self.weight = (sum_Xy - Xy_byN) / (sum_X_square - X_square_byN)
-            self.bias = y_mean - self.weight * X_mean
+            if self.verbose and (not iteration % 100 or
+                                 iteration == self.iters - 1):
+                info = (
+                    f"Iteration {iteration}: "
+                    f"Weight={self.weight}, Bias={self.bias}"
+                )
+                print(info)
 
     def predict(self, X):
         return self.weight * X + self.bias
