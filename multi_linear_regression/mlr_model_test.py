6f3380adcc5f395eed13ee4e91cfa6897231e74a
Zenoix <jeff.wang0296@gmail.com>
2021-02-11 21:17:47 +1300

    Updated test

--- multi_linear_regression/mlr_model_test.py
+++ multi_linear_regression/mlr_model_test.py
@@ -1,29 +1,21 @@
 # import libraries
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-import matplotlib.pyplot as plt
 import random
 
 # import the model
-from slr_model import SimpleLinearRegression
+from mlr_model import LinearRegression
 
 # generate linear data
 X, y, coef = datasets.make_regression(
-    n_samples=1000, n_features=1, noise=20, coef=True)
-
-# need to randomly make negative data sometimes
-negative = random.choice([1, -1])
-y, coef = y * negative, coef * negative
-
-# data's coefficient
-print(coef)
+    n_samples=10, n_features=random.randint(2, 15), noise=20, coef=True)
 
 # split data for training and testing
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2)
 
 # create the model
-regressor = SimpleLinearRegression()
+regressor = LinearRegression()
 
 # train the model
 regressor.fit(X_train, y_train)
@@ -36,9 +28,3 @@ y_hat = regressor.predict(X_test)
 
 # print mse
 print(regressor.mse(y_test, y_hat))
-
-# graph the regression line
-pred_line = regressor.predict(X)
-plt.scatter(X, y)
-plt.plot(X, pred_line, color='black', linewidth=2)
-plt.show()
