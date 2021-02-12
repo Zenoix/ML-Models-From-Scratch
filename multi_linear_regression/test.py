ca9599b86dcbeaf70b973e36b15cc3f033d964fe
Zenoix <jeff.wang0296@gmail.com>
2021-02-12 15:34:38 +1300

    temp upload

---
+++ multi_linear_regression/test.py
@@ -0,0 +1,33 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+
+
+
+# %%
+# import the model
+from mlr_model import LinearRegression
+
+
+# %%
+X, y, coef = datasets.make_regression(
+    n_samples=10, n_features=2, noise=20, coef=True)
+
+
+# %%
+# split data for training and testing
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2)
+
+
+# %%
+lr = LinearRegression(verbose=True)
+lr.fit(X_train, y_train)
+
+
+# %%
+print(lr.params())
+
+print(coef)
