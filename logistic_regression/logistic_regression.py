cb280678ca7fb5ff7dfc6f93deeef610d589e724
Zenoix <jeff.wang0296@gmail.com>
2021-02-19 20:08:25 +1300

    Initialised hyperparams and updated __str__ method

--- logistic_regression/logistic_regression.py
+++ logistic_regression/logistic_regression.py
@@ -2,8 +2,17 @@ import numpy as np
 
 
 class Logistic_Regression:
-    def __init__(self):
-        pass
+    def __init__(self, learning_rate=0.01, iterations=1000, verbose=False):
+        # initialise hyperparameters
+        self.l_rate = learning_rate
+        self.iters = iterations
+        self.verbose = verbose
+        self.weights = None
+        self.bias = None
 
     def __str__(self):
-        return f"Logistic Regression Model"
+        output = (
+            f"Logistic Regression Model: "
+            f"learning rate={self.l_rate}, iterations={self.iters}"
+        )
+        return output
